57343620a06d8ba07c0a93d6d2012f0b31a25de2
yhbrothers <admin@yhbrothers.com>
2018-01-20 10:32:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/send',(req, res)=>{
         secure: false, // true for 465, false for other ports
         auth: {
             user: 'noreply@mylocal.help', // generated ethereal user
-            pass: ''  // generated ethereal password
+            pass: 'Miao196309$'  // generated ethereal password
         },
         tls:{
             rejectUnauthorized: false
